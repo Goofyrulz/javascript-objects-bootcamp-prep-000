9752450e39d1c4c7e743e391ce1dbfcde84a62a0
Christopher Anderson <andersonchris227@gmail.com>
2018-08-26 19:10:11 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,5 +2,7 @@ var playlist = {
  'Slowdive' : 'Alison',
  'My Bloody Valentine' : 'Sometimes'
 }
-function updatePlaylist(playlist, artist, song)
+function updatePlaylist(playlist, artist, song) {
+  playlist(artist) = song
+}
 
